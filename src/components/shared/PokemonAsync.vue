3e4e4c38f64fab843970fe0650410aaf1b7a0277
Alex <a.lee.sprague@gmail.com>
2026-05-10 09:20:48 -0500

    Got the pokemon async function working

--- src/components/shared/PokemonAsync.vue
+++ src/components/shared/PokemonAsync.vue
@@ -1,21 +1,105 @@
 <template>
+    <div class="pokemon-card">
 
+        <div class="sprite-container">
+            <img :src="pokeSprite" alt="">
+        </div>
+
+        <div class="pokemon-info">
+            <div class="top-row">
+                <span class="name">{{ pokeName }}</span>
+                <span class="id">#{{ pokeId }}</span>
+            </div>
+
+            <div class="bottom-row">
+                <span>{{ pokeType1 }}</span>
+                <span>{{ pokeType2 }}</span>
+            </div>
+        </div>
+
+    </div>
 </template>
 
-<script>
+<style scoped>
+.pokemon-card {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+
+    background-color: #1e1e1e;
+    border: 2px solid #333;
+
+    padding: 0.75rem 1rem;
+    border-radius: 12px;
+
+    color: white;
+
+    min-width: 250px;
+}
+
+.sprite-container img {
+    width: 80px;
+    height: 80px;
+}
+
+.pokemon-info {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    height: 80px;
+}
+
+.top-row,
+.bottom-row {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+}
+
+.name {
+    font-weight: bold;
+    text-transform: capitalize;
+    font-size: 1.1rem;
+}
+
+.id {
+    color: #999;
+}
+
+.bottom-row span {
+    background-color: #333;
+    padding: 0.25rem 0.5rem;
+    border-radius: 6px;
+
+    text-transform: capitalize;
+    font-size: 0.9rem;
+}
+</style>
+
+<script setup>
+import {ref} from 'vue'
+
+let pokeSprite = ref("");
+let pokeName = ref("");
+let pokeId = ref("");
+let pokeType1 = ref("");
+let pokeType2 = ref("");
+
+
 async function callPokemonApi() {
     let randomPokemonId = Math.floor(Math.random() * 1028) + 1;
     const apiCall = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}/`)
     let pokeJson = await apiCall.json();
-    let pokeSprite = pokeJson.sprites.back_default;;
-    let pokeName = pokeJson.species.name;;
-    let pokeId = pokeJson.id;
-    let pokeType1 = pokeJson.types[0].type.name;
+    pokeSprite.value = pokeJson.sprites.front_default;
+    pokeName.value = pokeJson.species.name;
+    pokeId.value = pokeJson.id;
+    pokeType1.value = pokeJson.types[0].type.name;
     // Optional second type
-    let pokeType2 =
+    pokeType2.value =
         pokeJson.types[1]
             ? pokeJson.types[1].type.name
-            : "None";
+            : "";
 
     console.log(pokeSprite);
     console.log(pokeName);
